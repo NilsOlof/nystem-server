3d320a69b99676e30ae95d07c8244467db3f8293
Nisse <nisse@lampyrine.com>
2020-10-07 21:19:28 +0200

    Build

---
+++ build/precache-manifest.b2b6c5806a707d9842987c8219c56738.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afba9b5506a0a0c42d9f41337520556a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f6b19208f6c7e2a0701",
+    "url": "/static/css/2.9a6de410.chunk.css"
+  },
+  {
+    "revision": "b7d18d3f331ae2b7cce3",
+    "url": "/static/css/main.22b6cd0d.chunk.css"
+  },
+  {
+    "revision": "8f6b19208f6c7e2a0701",
+    "url": "/static/js/2.6cacc659.chunk.js"
+  },
+  {
+    "revision": "e6314c67e777e94f4caf66d8d58169a0",
+    "url": "/static/js/2.6cacc659.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7d18d3f331ae2b7cce3",
+    "url": "/static/js/main.82cbb747.chunk.js"
+  },
+  {
+    "revision": "d644ff198816bb9f0669",
+    "url": "/static/js/runtime-main.c0627188.js"
+  }
+]);
