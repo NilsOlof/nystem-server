6b4071ec87b00c14ce21e250cfdec5e4eed2389a
Nisse <nisse@lampyrine.com>
2021-11-23 23:18:31 +0100

    Build

---
+++ build/precache-manifest.6f4be17c87c120ec74b6cb8aaae1cbbc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b2e601df440bee4f14cca6409115d39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab9ba21196ad11a10053",
+    "url": "/static/css/3.08b435ed.chunk.css"
+  },
+  {
+    "revision": "2aff7212a8236b4b7e9a",
+    "url": "/static/css/main.c1538475.chunk.css"
+  },
+  {
+    "revision": "18dc4aabd466080f0d7f",
+    "url": "/static/js/2.b69c91c7.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/static/js/2.b69c91c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab9ba21196ad11a10053",
+    "url": "/static/js/3.ca186cfc.chunk.js"
+  },
+  {
+    "revision": "d2515f99ecca6a2a93d42551bdfdcaab",
+    "url": "/static/js/3.ca186cfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31537b9d9457feae90b4",
+    "url": "/static/js/4.261d261b.chunk.js"
+  },
+  {
+    "revision": "2aff7212a8236b4b7e9a",
+    "url": "/static/js/main.01270e67.chunk.js"
+  },
+  {
+    "revision": "690ee05db71f96431fc5",
+    "url": "/static/js/runtime-main.ebe6a727.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f2954ac1b529e11af9939f6f160fdbb1.js"
+  "/precache-manifest.6f4be17c87c120ec74b6cb8aaae1cbbc.js"
 );
 
 self.addEventListener('message', (event) => {
