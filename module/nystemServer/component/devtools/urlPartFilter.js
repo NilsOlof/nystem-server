217bcaa92ca4875745a8b741b8039ba294013346
Nisse <nisse@lampyrine.com>
2022-03-22 21:14:27 +0100

    Fixes

--- module/nystemServer/component/devtools/urlPartFilter.js
+++ module/nystemServer/component/devtools/urlPartFilter.js
@@ -11,10 +11,11 @@ const DevtoolsUrlPartFilter = ({ model, view }) => {
   useEffect(() => {
     const { tabId: id } = window.chrome.devtools.inspectedWindow;
 
-    window.chrome.tabs.get(id, (tab) => setUrl(tab.url));
+    window.chrome.tabs.get(id, (tab) => setUrl(tab.url || ""));
 
-    const onChange = (tabId, changeInfo) => {
-      if (tabId === id) setUrl(changeInfo.url);
+    const onChange = (tabId) => {
+      if (tabId === id)
+        window.chrome.tabs.get(id, (tab) => setUrl(tab.url || ""));
     };
     window.chrome.tabs.onUpdated.addListener(onChange);
 
@@ -25,7 +26,13 @@ const DevtoolsUrlPartFilter = ({ model, view }) => {
 
   return (
     <SelectInput
-      model={{ ...model, option: url.split("/") }}
+      model={{
+        ...model,
+        option: url.split("/").slice(3),
+        inline: true,
+        limit: 1,
+        render: "button",
+      }}
       value={value}
       setValue={setValue}
     />
