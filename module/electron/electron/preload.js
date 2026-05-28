f012e43833fb6dcd97aed208f2abb30f88a1aa10
Nisse <nisse@lampyrine.com>
2026-05-28 17:15:16 +0200

    Pre update

--- module/electron/electron/preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-contextBridge.exposeInMainWorld("electron", {
-  send: (msg) => ipcRenderer.send("msg", msg),
-  on: (callback) => ipcRenderer.on("msg", callback),
-  off: (callback) => ipcRenderer.removeListener("msg", callback),
-});
